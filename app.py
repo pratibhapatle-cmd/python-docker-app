c01e528f57553ae1d72b370e9510f23fa0324d97
Pratibha <pratibha.patle@gmail.com>
2025-12-23 01:10:17 +0000

    Use environment variable in python app

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 print("Hello from Dockerized Python app!")
 print("Hello from pratibha. I changed the code")
-print("Enter new string ")
+
+import os
+
+name = os.getenv("APP_NAME", "Docker App")
+
+print(f"Hello from {name}!")
